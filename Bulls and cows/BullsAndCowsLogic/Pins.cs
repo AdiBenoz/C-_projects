2dde401a48e29e6f6db6c1d0e9c5ce7d968fbba3
AdiBenoz <54435831+AdiBenoz@users.noreply.github.com>
2019-09-09 21:24:05 +0300

    Add files via upload

---
+++ Bulls and cows/BullsAndCowsLogic/Pins.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BullsAndCowsLogic
+{
+    public class Pins
+    {
+        private List<Utils.ePins> m_Pins = new List<Utils.ePins>();
+
+        //construct pins object for computer sequnce
+        //called only once - when initialize a new game
+        public Pins()
+        {
+            Random random = new Random();
+            Array valusOfPins = Enum.GetValues(typeof(Utils.ePins));
+            Utils.ePins randomPin = (Utils.ePins)valusOfPins.GetValue(random.Next(valusOfPins.Length));
+
+            for (int i = 0; i < Utils.k_NumberOfPins; i++)
+            {
+                while (this.m_Pins.Contains(randomPin))
+                {
+                    randomPin = (Utils.ePins)valusOfPins.GetValue(random.Next(valusOfPins.Length));
+                }
+                this.m_Pins.Add(randomPin);
+            }
+        }
+
+        //construct pins object for user guess
+        public Pins(List<Utils.ePins> i_UserGuess)
+        {
+            foreach (Utils.ePins guess in i_UserGuess)
+            {
+                m_Pins.Add(guess);
+            }
+        }
+
+        public List<Utils.ePins> Sequence
+        {
+            get
+            {
+                return this.m_Pins;
+            }
+        }
+    }
+}
